29a0945225db382e6253fdcc5a8d82ae1f31c5c5
Jakub Rejmann <kubarejmann@wp.pl>
2020-12-14 23:43:08 +0100

    Styling improved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
+import "./App.css";
+import MobileSVG from "./images/mobile.svg";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/analytics";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -23,12 +26,16 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-export default function App() {
+function App() {
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header>
+        <h1>Fire Chat 🔥</h1>
+        <SignOut />
+      </header>
+
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
@@ -40,23 +47,34 @@ function SignIn() {
     auth.signInWithPopup(provider);
   };
 
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+  return (
+    <>
+      <img src={MobileSVG} alt="mobileSVG" className="mobile-svg" />
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </>
+  );
 }
 
 function SignOut() {
   return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
   );
 }
 
 function ChatRoom() {
+  const dummy = useRef();
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
 
   const [messages] = useCollectionData(query, { idField: "id" });
-  const [formValue, setFormValue] = useState("");
 
-  const dummy = useRef();
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -69,8 +87,8 @@ function ChatRoom() {
       uid,
       photoURL,
     });
-    setFormValue("");
 
+    setFormValue("");
     dummy.current.scrollIntoView({ behavior: "smooth" });
   };
 
@@ -80,29 +98,41 @@ function ChatRoom() {
         {messages &&
           messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-        <div ref={dummy}></div>
+        <span ref={dummy}></span>
       </main>
 
       <form onSubmit={sendMessage}>
         <input
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Aa"
         />
-        <button type="submit">Send</button>
+
+        <button type="submit" disabled={!formValue}>
+          Send
+        </button>
       </form>
     </>
   );
+}
 
-  function ChatMessage(props) {
-    const { text, uid, photoURL } = props.message;
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
 
-    const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-    return (
+  return (
+    <>
       <div className={`message ${messageClass}`}>
-        <img src={photoURL} alt="photoURL" />
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+        />
         <p>{text}</p>
       </div>
-    );
-  }
+    </>
+  );
 }
+
+export default App;
